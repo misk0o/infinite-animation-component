8729de7e011531740f1603e535ca0508f47a22bb
misk0o <michal.pipa.14@gmail.com>
2026-02-25 01:21:02 +0100

    pridany text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   if (isLoading) {
     return (
       <HaloLoader
-        text=""
+        text="misk0"
         onComplete={() => {
           setTimeout(() => setIsLoading(false), 500);
         }}
